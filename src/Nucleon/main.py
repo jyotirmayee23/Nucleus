f4991427484bd5b5de67ee39291e5a8ee6c123e6
jyotirmayee <156336518+jyotirmayee23@users.noreply.github.com>
2024-08-21 15:10:16 +0530

    Update main.py

--- src/Nucleon/main.py
+++ src/Nucleon/main.py
@@ -3,21 +3,24 @@ from io import BytesIO
 import json
 import datetime
 import os
-# import PyPDF2
-from pdf2image import convert_from_path
+import fitz 
 from PIL import Image
+import pytesseract
 import tempfile
 from botocore.config import Config
 import requests
-from PIL import Image
 import io
+import uuid
 from langchain.chains import LLMMathChain, LLMChain
 from langchain.agents.agent_types import AgentType
 from langchain.agents import Tool, initialize_agent
 from langchain.prompts import PromptTemplate
 from langchain_aws import ChatBedrock
 
-images_dir = '/tmp/images'
+base_temp_dir = '/tmp/images'
+def ensure_directory_exists(directory_path):
+    """Ensures the directory exists."""
+    os.makedirs(directory_path, exist_ok=True)
 supported_image_formats = ('.jpeg', '.jpg', '.png')
 
 s3 = boto3.client('s3')
@@ -28,6 +31,10 @@ bedrock_runtime = boto3.client(
         service_name="bedrock-runtime",
         region_name="us-east-1",
     )
+
+# radiological_investigations_str = ", ".join(radiological_investigations)
+
+# response_text = "For Radiology charges, please refer to the following list of radiology investigations: " + radiological_investigations_str + ". Extract and sum the costs associated with these investigations."
 
 data_json = {
         "total bill amount": "",
@@ -49,73 +56,76 @@ data_json = {
 
 data_json_str = json.dumps(data_json)
 
+
 # @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event, context):
     job_id = event['job_id']
     links = event.get('links', [])
     print(f"Links received: {links}")
+    local_directory = "local_directory_path"
     image_text = ""
     pdf_image_text = ""
 
     # Download each file from the links
+    all_table_info = []
+    all_final_maps = {}
+    
+    
     for link in links:
         url_parts = link.split('/')
-        # print("url", url_parts)
         bucket_name1 = url_parts[2]
         if '.s3.amazonaws.com' in bucket_name1:
             bucket_name = bucket_name1.rstrip('.s3.amazonaws.com')
+            print("5")
         else:
             bucket_name = bucket_name1
 
-        print("er",bucket_name)
-
         object_key = '/'.join(url_parts[3:])
-        
-        print("key",object_key)
+        print(object_key)
+        local_file_path = os.path.join(local_directory, object_key)
 
         if object_key.lower().endswith(supported_image_formats):
-            # Call Textract to extract text from the image
-            textract_response = textract.detect_document_text(
-                Document={'S3Object': {'Bucket': bucket_name, 'Name': object_key}}
-            )
-
-            # Extract text blocks from the response
-            for block in textract_response['Blocks']:
-                if block['BlockType'] == 'LINE':
-                    image_text += block['Text'] + "\n"
-            # print("image_text_123", image_text)
+            # local_path = '/tmp/' + object_key
+            # print("lp",local_path)
+            # print("ok",object_key)
+            
+            local_file_id = str(uuid.uuid4())
+            local_directory = '/tmp/images'
+            local_file_path = os.path.join(local_directory, object_key.rsplit('/', 1)[0], local_file_id)
+            ensure_directory_exists(os.path.dirname(local_file_path))
+            s3.download_file(bucket_name, object_key, local_file_path)
+            image = Image.open(local_file_path)
+            text = pytesseract.image_to_string(image)
+            image_text += text + '\n'
+            # print(combined_text)
+            
+            # s3.download_file(bucket_name, object_key, f"{local_directory}/{object_key}")
         elif object_key.lower().endswith('.pdf'):
-            local_path = '/tmp/' + object_key.split('/')[-1]
-            print("Local path:", local_path)
-            s3.download_file(bucket_name, object_key, local_path)
-
-            images = convert_from_path(local_path)
+            local_file_id = str(uuid.uuid4())
+            local_directory = '/tmp/images'
+            local_file_path = os.path.join(local_directory, object_key.rsplit('/', 1)[0], local_file_id)
+            # local_pdf_path = '/tmp/' + object_key
+            print("ok",object_key)
+            print("lp",local_file_path)
+            ensure_directory_exists(os.path.dirname(local_file_path))
+            s3.download_file(bucket_name, object_key, local_file_path)
             base_name = os.path.splitext(object_key.split('/')[-1])[0]
-            print("base_name",base_name)
+            print("bn",base_name)
 
-            # Process each image without uploading to S3
-            for i, image in enumerate(images):
-                image_file_name = f'{base_name}_{i+1}.png'
-                # Save the image to a BytesIO object
-                with io.BytesIO() as buffer:
-                    image.save(buffer, format='PNG')
-                    buffer.seek(0)
-                    
-                    # Send image to Textract
-                    textract_response = textract.detect_document_text(
-                        Document={'Bytes': buffer.getvalue()}
-                    )
-                    
-                    # Extract text blocks from the response
-                    for block in textract_response['Blocks']:
-                        if block['BlockType'] == 'LINE':
-                            pdf_image_text += block['Text'] + "\n"
-                    # print(f"Extracted text from {image_file_name}:", pdf_image_text)
-            # print(f"Extracted text from {image_file_name}:", pdf_image_text)
+            pdf_document = fitz.open(local_file_path)
+            for page_number in range(len(pdf_document)):
+                page = pdf_document.load_page(page_number)
+                pix = page.get_pixmap()
+                output_image_path = f'/tmp/{base_name}_page_{page_number + 1}.png'
+                pix.save(output_image_path)
+
+                image = Image.open(output_image_path)
+                text = pytesseract.image_to_string(image)
+                pdf_image_text += text + '\n'
+            print("pit",pdf_image_text)
 
     combined_text = image_text + pdf_image_text
-    
-
+                
     body = json.dumps({
         "anthropic_version": "bedrock-2023-05-31",
         "max_tokens": 4000,
@@ -137,7 +147,15 @@ def lambda_handler(event, context):
                     {"type": "text", "text": "For Billing date , we only need to take the billing date of hospital bill only"},
                     {"type": "text", "text": "For Room charges include room charges + nursing charges also"},
                     {"type": "text", "text": "For Room type only get the type keyword"},
+                    {"type": "text", "text": "For 'Pathology charges,' only include the charges for the listed pathological investigations."},
+                    {"type": "text", "text": "extracted text are like key and then value in next line , so answer for any key will be most probably in the next value"},
+                    {"type": "text", "text": "For pharmacy charge incase check for all the total amount of pharmacy and then add it and then give total amount and ignore the discount part."},
+                    # {"type": "text", "text": "See for other charges we have to take the total calculation of all the bills passed which should be mentioned in total amount or total bill. Subtract the sum of the charges mentioned in the data_json_str (e.g., room charges, surgeon charges, etc.) from the total bill amount, and the result will be the value for 'other charges.'"},
                     {"type": "text", "text": "Only return the values for each json (dont include explanation or calculation)"},
+                    {"type": "text", "text": "Only return the values for each json and not explanation "},
+                    # {"type": "text", "text": "can u give the explanation of each value conclusion"},
+                    # {"type": "text", "text": response_text},
+                    # {"type": "text", "text": "For Radiology charges, please refer to the following list of radiology investigations: " + radiological_investigations + ". Extract and sum the costs associated with these investigations."},
                     # {"type": "text", "text": "For each entity, provide the extracted value and an explanation of how you arrived at this extraction. Include specific references to the text from the document that led to your extraction decision."},
                     #  {"type": "text", "text": "For each entity, provide the extracted value and a detailed explanation of how you arrived at this extraction. Include specific references to the text from the document that led to your extraction decision. For 'visit charges,' provide the exact line or section from the document used to determine the charge amount and verify the number of visits if mentioned. If the explanation includes calculations, ensure that they are based on verifiable document content."}
                 ],
